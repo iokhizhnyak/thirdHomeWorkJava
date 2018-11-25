ba77d228fc5d5059a6a5335c7feb315dfdd27cd3
iokhizhnyak <iokhizhnyak@gmail.com>
2018-11-25 21:32:42 +0200

    Initial commit

---
+++ src/Fairytale.java
@@ -0,0 +1,23 @@
+public class Fairytale {
+
+    public int pages;
+    public int heroes;
+    public int readers;
+
+    public Fairytale (int startPages, int startHeroes, int startReaders){
+        pages = startPages;
+        heroes = startHeroes;
+        readers = startReaders;
+    }
+
+    public void setHeroes(int newHeroes) {
+        heroes = newHeroes;
+    }
+
+  
+
+    public static void main(String[] args) {
+
+
+    }
+}
